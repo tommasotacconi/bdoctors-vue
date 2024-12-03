a34e2e362c26d1c43dbdaba213adccb652227027
jacopolegovini <legovini96@gmail.com>
2024-12-03 11:00:39 +0100

    improved style card

--- src/components/Dashboard/MainDashboardComponents/InformationPage.vue
+++ src/components/Dashboard/MainDashboardComponents/InformationPage.vue
@@ -21,9 +21,20 @@ export default {
                     <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title">Nome medico</h5>
-                            <p class="card-text">Prestazioni che propone</p>
-                            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <p class="card-text">
+                            <ul>
+                                <li>
+                                    <strong>Specializzazione:</strong> Cardiologia / Neurologia
+                                </li>
+                                <li>
+                                    <strong>Indirizzo:</strong> Via Boolean 15, Caltanisetta
+                                </li>
+                                <li>
+                                    <strong>Telefono:</strong> 338451523
+                                </li>
+                            </ul>
+                            </p>
+                            <a href="#" class="btn btn-primary">Modifica il tuo profilo</a>
                         </div>
                     </div>
                 </div>
@@ -41,8 +52,6 @@ h2 {
 .card {
     border-radius: 40px;
     background-color: #D8F9FF;
-    display: flex;
-    align-items: center;
     border: 0;
     text-align: center;
     width: 70%;
@@ -52,6 +61,18 @@ h2 {
     border-radius: 50%;
     width: 60%;
     border: 1px solid #65B0FF;
-    margin: 10px;
+    margin: 15px 0;
+}
+
+.card-body {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+ul {
+    text-align: start;
 }
 </style>
