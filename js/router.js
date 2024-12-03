d880e72cdf4453821245aea56040f9457155a92e
jacopolegovini <legovini96@gmail.com>
2024-12-03 10:22:13 +0100

    page and route added

--- js/router.js
+++ js/router.js
@@ -1,8 +1,10 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import Homepage from '../src/pages/Homepage.vue'
+import Dashboard from '../src/pages/Dashboard.vue'
 
 const routes = [
     { path: '/', component: Homepage, name: 'homepage' },
+    { path: '/user/:id', component: Dashboard, name: 'dashboard' },
 ]
 
 const router = createRouter({
---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,20 @@
+<script>
+import SidebarDashboard from '../components/Dashboard/SidebarDashboard.vue';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        SidebarDashboard,
+    }
+}
+</script>
+
+<template>
+    <SidebarDashboard />
+</template>
+
+<style scoped></style>
