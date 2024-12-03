cc9cf11d6d5d4c69dbdf403a89c04a02e006f5be
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-12-03 17:45:20 +0100

    added Login functionality and router path

--- js/router.js
+++ js/router.js
@@ -1,8 +1,10 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import Homepage from '../src/pages/Homepage.vue'
+import LoginPage from '../src/pages/LoginPage.vue'
 
 const routes = [
     { path: '/', component: Homepage, name: 'homepage' },
+    { path: '/Login', component: LoginPage, name: 'Login' },
 ]
 
 const router = createRouter({
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,24 @@
+<script>
+import AppHeader from '../components/Homepage/AppHeader.vue';
+import AppLogin from '../components/Login/AppLogin.vue'; 
+
+export default {
+	data() {
+		return {
+		  
+		}
+	},
+	components: {
+		AppHeader,
+		AppLogin,
+	}
+}
+</script>
+
+<template>
+	<AppHeader />
+	<AppLogin />
+</template>
+
+<style lang="scss" scoped>
+</style>
