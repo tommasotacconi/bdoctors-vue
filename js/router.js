574ee43b97cb9c54ce4469ee314284eaa03cdbeb
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-12-03 10:26:30 +0100

    added profile route to router js

--- js/router.js
+++ js/router.js
@@ -1,8 +1,10 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import Homepage from '../src/pages/Homepage.vue'
+import Profile from '../src/pages/Profile.vue'
 
 const routes = [
     { path: '/', component: Homepage, name: 'homepage' },
+    { path: '/profile', component: Profile, name: 'profile' },
 ]
 
 const router = createRouter({
---
+++ src/components/Profile/AppMainProfile.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Profilo</h1>
+</template>
+
+<style scoped></style>
