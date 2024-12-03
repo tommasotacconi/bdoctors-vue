b0fc12a2d958367cf3d33db229fa1e16daf15584
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-12-03 11:03:45 +0100

    added profile edit page

---
+++ src/pages/ProfileEdit.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="text-center">Modifica le tue informazioni</h1>
+
+        <form action="GET" @submit.prevent>
+
+
+        </form>
+    </div>
+
+</template>
+
+<style scoped></style>
