6ba6de053624225968a82ba4df030698fb9968f3
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-12-03 16:39:01 +0100

    edited validation and layout

--- src/components/Profile/AppProfileEdit.vue
+++ src/components/Profile/AppProfileEdit.vue
@@ -31,7 +31,7 @@ export default {
                 })
                 .catch((error) => {
                     this.errors = response.result.errors;
-                    console.log(response.result.errors);
+                    console.error(response.result.errors);
                 });
         },
     },
@@ -43,7 +43,7 @@ export default {
             })
             .catch(function (error) {
                 // handle error
-                console.log(error);
+                console.error(error);
             })
             .finally(function () {
                 // always executed
@@ -56,63 +56,64 @@ export default {
     <div class="container py-3">
         <h1 class="text-center">Modifica le tue informazioni</h1>
 
-        <form action="PUT" @submit="updateForm">
+        <form action="" method="PUT" @submit="updateForm" novalidate="true">
 
             <div v-if="errors.length" class="bg-subtle-danger">
-                <ul v-for="error in errors">
-                    <li>{{ error }}</li>
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
                 </ul>
             </div>
-
-            <div class="mb-3 col-3">
-                <label for="firstname" class="form-label">Nome</label>
-                <input type="text" class="form-control" id="firstname" v-model="formData.firstname">
+            <div class="row py-3">
+                <div class="mb-3 col-4">
+                    <label for="firstname" class="form-label">Nome</label>
+                    <input type="text" class="form-control" id="firstname" v-model="formData.firstname" required>
+                </div>
+                <div class="mb-3 col-4">
+                    <label for="lastname" class="form-label">Cognome</label>
+                    <input type="text" class="form-control" id="lastname" v-model="formData.lastname" required>
+                </div>
+                <div class="mb-3 col-4">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" v-model="formData.email" required>
+                </div>
+                <div class="mb-3 col-6">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" v-model="formData.password" required>
+                </div>
+                <div class="mb-3 col-6">
+                    <label for="phone" class="form-label">Telefono</label>
+                    <input type="tel" class="form-control" id="phone" v-model="formData.phone" required>
+                </div>
+                <div class="mb-3 col-6">
+                    <label for="officeAddress" class="form-label">Indirizzo</label>
+                    <input type="text" class="form-control" id="officeAddress" v-model='formData.officeAddress'
+                        required>
+                </div>
+                <div class="mb-3 col-6">
+                    <label for="specialization" class="form-label">Specializzazioni</label>
+                    <select class="form-select" aria-label="Default select example" id="specialization"
+                        v-model="formData.specialization" required>
+                        <option disabled>Seleziona la/e tua/e specializzazione/i</option>
+                        <option value="surgery">Chirurgia</option>
+                        <option value="cardiology">Cardiologia</option>
+                        <option value="ophthalmology">Oculistica</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="services" class="form-label">Prestazioni</label>
+                    <textarea class="form-control" id="services" v-model="formData.services" required></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="photo" class="form-label">Foto profilo</label>
+                    <input type="url" class="form-control" id="photo" placeholder="Inserisci un URL valido"
+                        v-model="formData.photo" required>
+                </div>
+                <div class="mb-3">
+                    <label for="curriculum" class="form-label">Curriculum Vitae</label>
+                    <input type="url" class="form-control" id="curriculum" placeholder="Inserisci un URL valido"
+                        v-model="formData.curriculum" required>
+                </div>
             </div>
-            <div class="mb-3 col-3">
-                <label for="lastname" class="form-label">Cognome</label>
-                <input type="text" class="form-control" id="lastname" v-model="formData.lastname">
-            </div>
-            <div class="mb-3 col-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" v-model="formData.email">
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" v-model="formData.password">
-            </div>
-            <div class="mb-3">
-                <label for="phone" class="form-label">Telefono</label>
-                <input type="tel" class="form-control" id="phone" v-model="formData.phone">
-            </div>
-            <div class="mb-3">
-                <label for="officeAddress" class="form-label">Indirizzo</label>
-                <input type="text" class="form-control" id="officeAddress" v-model='formData.officeAddress'>
-            </div>
-            <div class="mb-3">
-                <label for="specialization" class="form-label">Specializzazioni</label>
-                <select class="form-select" aria-label="Default select example" id="specialization"
-                    v-model="formData.specialization">
-                    <option selected>Seleziona la/e tua/e specializzazione/i</option>
-                    <option value="surgery">Chirurgia</option>
-                    <option value="cardiology">Cardiologia</option>
-                    <option value="ophthalmology">Oculistica</option>
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="services" class="form-label">Prestazioni</label>
-                <textarea class="form-control" id="services" v-model="formData.services"></textarea>
-            </div>
-            <div class="mb-3">
-                <label for="photo" class="form-label">Foto profilo</label>
-                <input type="url" class="form-control" id="photo" placeholder="Inserisci un URL valido"
-                    v-model="formData.photo">
-            </div>
-            <div class="mb-3">
-                <label for="curriculum" class="form-label">Curriculum Vitae</label>
-                <input type="url" class="form-control" id="curriculum" placeholder="Inserisci un URL valido"
-                    v-model="formData.curriculum">
-            </div>
-
 
             <button type="submit" class="btn btn-primary me-2">Modifica</button>
             <button type="reset" class="btn btn-dark">Reset</button>
