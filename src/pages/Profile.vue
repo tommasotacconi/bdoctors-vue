a74e6c8375fae17dcc91aa5f7afa0657987aca89
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-12-03 10:24:26 +0100

    added profile page

---
+++ src/pages/Profile.vue
@@ -0,0 +1,25 @@
+<script>
+import AppHeader from "../components/Homepage/AppHeader.vue";
+import AppMainProfile from "../components/Profile/AppMainProfile.vue";
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        AppHeader,
+        AppMainProfile
+    }
+
+
+}
+</script>
+
+<template>
+    <AppHeader />
+    <AppMainProfile />
+</template>
+
+<style scoped></style>
