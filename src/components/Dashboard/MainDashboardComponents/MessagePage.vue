1700a4d4b639f156151d530870426ce76cead9b3
jacopolegovini <legovini96@gmail.com>
2024-12-03 15:04:01 +0100

    inbox card draft completed

--- src/components/Dashboard/MainDashboardComponents/MessagePage.vue
+++ src/components/Dashboard/MainDashboardComponents/MessagePage.vue
@@ -13,20 +13,65 @@ export default {
 <template>
     <main class="container">
         <div class="general-cards-container">
+            <div class="last-message">
+
+            </div>
             <div class="messages">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-header">
-                        Featured
+                <div class="card">
+                    <div class="card-header-title">
+                        <h5 class="title">Messaggi arrivati</h5>
+                        <div class="messages-number">Totale: 3</div>
                     </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">An item</li>
-                        <li class="list-group-item">A second item</li>
-                        <li class="list-group-item">A third item</li>
-                    </ul>
+                    <div class="card-body-list" v-for="(message, index) in 3">
+                        <ul class="list-group list-group-flush list-email">
+                            <li class="list-group-item">E-mail</li>
+                        </ul>
+                        <ul class="list-group list-group-flush list-name">
+                            <li class="list-group-item">Nome e Cognome</li>
+                        </ul>
+                        <ul class="list-group list-group-flush list-preview">
+                            <li class="list-group-item">Preview</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
     </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* Inbox Card */
+.card {
+    border-radius: 20px;
+    padding: 10px;
+}
+
+.card-header-title {
+    display: flex;
+    gap: 10px;
+}
+
+.card-body-list {
+    display: flex;
+}
+
+.list-group {
+    border-right: 3px solid lightgray;
+    border-bottom: 3px solid lightgray;
+
+}
+
+.list-email {
+    flex-basis: 20%;
+}
+
+.list-name {
+    flex-basis: 20%;
+
+}
+
+.list-preview {
+    flex-basis: 60%;
+
+}
+</style>
