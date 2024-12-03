29e1514f7e92c4be34ef36c81d908b7f13989e86
jacopolegovini <legovini96@gmail.com>
2024-12-03 14:34:15 +0100

    lorem ipsum message

--- src/components/Dashboard/MainDashboardComponents/MessagePage.vue
+++ src/components/Dashboard/MainDashboardComponents/MessagePage.vue
@@ -11,7 +11,6 @@ export default {
 </script>
 
 <template>
-    <div>{{ store.messagePage }}</div>
     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil facilis ut repudiandae quod, in eum quia fugiat,
     fuga nulla quo est et ducimus iste dolore! Vitae voluptate aperiam nulla neque?
 </template>
