cf2c87dd354e44a505ebd0aad1c8c128ab1c0b6f
jacopolegovini <legovini96@gmail.com>
2024-12-04 10:33:07 +0100

    logout implemented

--- src/components/Dashboard/HeaderDashboard.vue
+++ src/components/Dashboard/HeaderDashboard.vue
@@ -4,12 +4,21 @@ import { store } from '../../../js/store.js'
 export default {
     data() {
         return {
-
+            logout: false
         }
     },
     methods: {
         toggleSidebar() {
             store.sidebar = !store.sidebar
+        },
+        showLogout() {
+            this.logout = !this.logout;
+
+            if (this.logout === true) {
+                setTimeout(() => {
+                    this.logout = !this.logout
+                }, 5000)
+            }
         }
     }
 }
@@ -21,7 +30,10 @@ export default {
             <button @click="toggleSidebar"><i class="fa-solid fa-bars"></i></button>
         </div>
         <div class="user">
-            <i class="fa-solid fa-user"></i>
+            <div class="logout" v-if="logout">
+                <router-link to="/"><span class="logout-text">Logout</span></router-link>
+            </div>
+            <i class="fa-solid fa-user" @click="showLogout"></i>
         </div>
     </header>
 </template>
@@ -48,6 +60,12 @@ button {
     font-size: 2rem;
 }
 
+.user {
+    display: flex;
+    gap: 15px;
+    align-items: center;
+}
+
 .fa-user {
     border-radius: 50%;
     border: 1px solid white;
@@ -55,5 +73,18 @@ button {
     background-color: white;
     color: var(--color-complementary);
     margin-right: 7px;
+    height: 100%
+}
+
+.logout {
+    padding: 10px 15px;
+    background-color: var(--color-complementary);
+    border-radius: 30px;
+}
+
+.logout-text {
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
 }
 </style>
