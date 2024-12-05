61359b1439420e7b001a14359c53f8cf37e70960
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-12-05 12:20:04 +0100

    added create page

---
+++ src/pages/Createpage.vue
@@ -0,0 +1,20 @@
+<script>
+import AppCreate from '../components/Create/AppCreate.vue';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        AppCreate
+    }
+}
+</script>
+
+<template>
+    <AppCreate />
+</template>
+
+<style scoped lang="scss"></style>
