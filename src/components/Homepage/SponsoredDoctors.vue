b31249b401b0226ecbbc4fa942255ed628eda9ea
jacopolegovini <legovini96@gmail.com>
2024-12-09 16:41:03 +0100

    homepage style improved

--- src/components/Homepage/SponsoredDoctors.vue
+++ src/components/Homepage/SponsoredDoctors.vue
@@ -11,9 +11,9 @@ export default {
 <template>
     <main class="mt-4">
         <div class="container sponsored-doctor-container">
-            <h2>Medici in evidenza</h2>
-            <div class="card-container">
-                <div class="card d-flex" style="width: 18rem;" v-for="doctor in 5">
+            <h2>Dottori in evidenza</h2>
+            <div class="sponsored-card-container">
+                <div class="card card-sponsored d-flex" style="width: 18rem;" v-for="doctor in 5">
                     <img src="https://media.istockphoto.com/id/1340883379/photo/young-doctor-hospital-medical-medicine-health-care-clinic-office-portrait-glasses-man.jpg?s=612x612&w=0&k=20&c=_H4VUPBkS0gEj5ZdZzQo-Hw3lMuyofJpB-P9yS92Wyw="
                         class="card-img-top" alt="...">
                     <div class="card-body">
@@ -24,11 +24,13 @@ export default {
                 </div>
             </div>
         </div>
+
     </main>
 </template>
 
 <style scoped>
-h2 {
+/* Sponsored Doctor */
+.sponsored-doctor-container h2 {
     text-align: center;
     color: white;
     margin-bottom: 20px;
@@ -38,27 +40,29 @@ h2 {
     background-color: var(--color-complementary);
     border-radius: 25px;
     padding: 20px;
+    margin-bottom: 50px;
 }
 
-.card-container {
+.sponsored-card-container {
     display: flex;
     gap: 20px;
     justify-content: center;
 }
 
-.card {
-    border-radius: 40px;
+.card-sponsored {
     background-color: var(--color-secondary);
+    border-radius: 40px;
     flex-direction: column;
     align-items: center;
     border: 0;
     text-align: center;
+    flex-basis: 240px;
 }
 
 .card img {
     border-radius: 50%;
     width: 70%;
-    border: 1px solid #65B0FF;
+    border: 1px solid var(--color-primary);
     margin-top: 10px;
 }
 </style>
