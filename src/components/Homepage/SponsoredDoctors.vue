9fbc49f6669832b63ef1d5ad3fb51254dc29e34a
jacopolegovini <legovini96@gmail.com>
2024-12-09 16:29:59 +0100

    sponsored doctor highlight style

--- src/components/Homepage/SponsoredDoctors.vue
+++ src/components/Homepage/SponsoredDoctors.vue
@@ -10,15 +10,17 @@ export default {
 
 <template>
     <main class="mt-4">
-        <div class="container">
+        <div class="container sponsored-doctor-container">
             <h2>Medici in evidenza</h2>
-            <div class="card d-flex" style="width: 18rem;">
-                <img src="https://media.istockphoto.com/id/1340883379/photo/young-doctor-hospital-medical-medicine-health-care-clinic-office-portrait-glasses-man.jpg?s=612x612&w=0&k=20&c=_H4VUPBkS0gEj5ZdZzQo-Hw3lMuyofJpB-P9yS92Wyw="
-                    class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Nome medico</h5>
-                    <p class="card-text">Prestazioni medico</p>
-                    <a href="#" class="btn btn-primary">Profilo medico</a>
+            <div class="card-container">
+                <div class="card d-flex" style="width: 18rem;" v-for="doctor in 5">
+                    <img src="https://media.istockphoto.com/id/1340883379/photo/young-doctor-hospital-medical-medicine-health-care-clinic-office-portrait-glasses-man.jpg?s=612x612&w=0&k=20&c=_H4VUPBkS0gEj5ZdZzQo-Hw3lMuyofJpB-P9yS92Wyw="
+                        class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Nome medico</h5>
+                        <p class="card-text">Specializzazioni medico</p>
+                        <a href="#" class="btn btn-primary">Profilo medico</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -28,11 +30,25 @@ export default {
 <style scoped>
 h2 {
     text-align: center;
+    color: white;
+    margin-bottom: 20px;
+}
+
+.sponsored-doctor-container {
+    background-color: var(--color-complementary);
+    border-radius: 25px;
+    padding: 20px;
+}
+
+.card-container {
+    display: flex;
+    gap: 20px;
+    justify-content: center;
 }
 
 .card {
     border-radius: 40px;
-    background-color: #D8F9FF;
+    background-color: var(--color-secondary);
     flex-direction: column;
     align-items: center;
     border: 0;
