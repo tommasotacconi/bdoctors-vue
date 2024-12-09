7de5065b56f034444001b40c5555e7b8d4c62e29
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-12-09 12:36:23 +0100

    Revert "trying csrf authentication"
    This reverts commit 9a6bf57cb01801bfff8fb07204938d445f0d9c08.

--- js/main.js
+++ js/main.js
@@ -2,15 +2,5 @@ import { createApp } from 'vue'
 import App from '../src/App.vue'
 import { router } from './router'
 import 'bootstrap/dist/css/bootstrap.css'
-import axios from 'axios'
 
-// Configure axios defaults before making requests
-axios.defaults.withCredentials = true
-axios.defaults.baseURL = 'http://localhost:8000/' // backend URL
-axios.defaults.withXSRFToken = true
-
-// Get CSRF cookie
-await axios.get('sanctum/csrf-cookie')
-
-// Create and mount Vue app
 createApp(App).use(router).mount('#app')
