d5d210e1788aec6a702039504408ca260a1813b6
jacopolegovini <legovini96@gmail.com>
2024-12-09 11:35:22 +0100

    loader added

--- src/components/Search/SearchedDoctor.vue
+++ src/components/Search/SearchedDoctor.vue
@@ -12,6 +12,7 @@ export default {
             specializationId: store.searchedSpecialization,
             doctors: [],
             specializationName: '',
+            loaded: false,
         }
     },
     methods: {
@@ -65,41 +66,54 @@ export default {
 
         }
     },
+    computed: {
+        showLoader() {
+            setTimeout(() => {
+                this.loaded = true
+            }, 2000)
+        }
+    },
     created() {
         this.getApiProfile()
         this.getSpecializationName()
+    },
+    mounted() {
+        this.showLoader
     }
 }
 </script>
 
 <template>
     <main class="container">
-        <div class="title">
-            <h2>Ricerca per: {{ specializationName }}
-            </h2>
-            <!-- It's not working -->
-            <!-- {{ doctors[0].user.specializations[0].name }} -->
-        </div>
-        <div class="doctors-list">
-            <div class="doctor-card" v-for="doctor in doctors">
-                <img src="https://media.istockphoto.com/id/1340883379/photo/young-doctor-hospital-medical-medicine-health-care-clinic-office-portrait-glasses-man.jpg?s=612x612&w=0&k=20&c=_H4VUPBkS0gEj5ZdZzQo-Hw3lMuyofJpB-P9yS92Wyw="
-                    class="doctor-photo" alt="doctor photo">
-                <section class="doctor-information">
-                    <h5 class="doctor-name">
-                        {{ doctor.user.first_name }} {{ doctor.user.last_name }}
-                    </h5>
-                    <div class="doctor-address">
-                        <strong>Ufficio:</strong> {{ doctor.office_address }}
-                    </div>
-                    <div class="doctor-services">
-                        <strong>Prestazioni:</strong> {{ doctor.services }}
-                    </div>
-                </section>
+        <div class="loader" v-if="!loaded"></div>
+        <div v-if="loaded">
+            <div class="title">
+                <h2>Ricerca per: {{ specializationName }}
+                </h2>
+                <!-- It's not working -->
+                <!-- {{ doctors[0].user.specializations[0].name }} -->
+            </div>
+            <div class="doctors-list">
+                <div class="doctor-card" v-for="doctor in doctors">
+                    <img src="https://media.istockphoto.com/id/1340883379/photo/young-doctor-hospital-medical-medicine-health-care-clinic-office-portrait-glasses-man.jpg?s=612x612&w=0&k=20&c=_H4VUPBkS0gEj5ZdZzQo-Hw3lMuyofJpB-P9yS92Wyw="
+                        class="doctor-photo" alt="doctor photo">
+                    <section class="doctor-information">
+                        <h5 class="doctor-name">
+                            {{ doctor.user.first_name }} {{ doctor.user.last_name }}
+                        </h5>
+                        <div class="doctor-address">
+                            <strong>Ufficio:</strong> {{ doctor.office_address }}
+                        </div>
+                        <div class="doctor-services">
+                            <strong>Prestazioni:</strong> {{ doctor.services }}
+                        </div>
+                    </section>
 
-                <!-- We can put the router link all over the card -->
-                <!-- <section class="profile-show">
-                    <button class="button-profile-show">Profilo</button>
-                </section> -->
+                    <!-- We can put the router link all over the card -->
+                    <!-- <section class="profile-show">
+                        <button class="button-profile-show">Profilo</button>
+                    </section> -->
+                </div>
             </div>
         </div>
     </main>
@@ -154,5 +168,41 @@ img {
     color: var(--color-primary);
     font-weight: bold;
     border: 1px solid var(--color-primary);
+}
+
+
+/* Loader progressive */
+.loader {
+    --r1: 154%;
+    --r2: 68.5%;
+    width: 60px;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    background:
+        radial-gradient(var(--r1) var(--r2) at top, #0000 79.5%, var(--color-secondary) 80%),
+        radial-gradient(var(--r1) var(--r2) at bottom, var(--color-secondary) 79.5%, #0000 80%),
+        radial-gradient(var(--r1) var(--r2) at top, #0000 79.5%, var(--color-secondary) 80%),
+        #ccc;
+    background-size: 50.5% 220%;
+    background-position: -100% 0%, 0% 0%, 100% 0%;
+    background-repeat: no-repeat;
+    animation: l9 2s infinite linear;
+    position: absolute;
+    top: 50%;
+    left: 59%;
+}
+
+@keyframes l9 {
+    33% {
+        background-position: 0% 33%, 100% 33%, 200% 33%
+    }
+
+    66% {
+        background-position: -100% 66%, 0% 66%, 100% 66%
+    }
+
+    100% {
+        background-position: 0% 100%, 100% 100%, 200% 100%
+    }
 }
 </style>
