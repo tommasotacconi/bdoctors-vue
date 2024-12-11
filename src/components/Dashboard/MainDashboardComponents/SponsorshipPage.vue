77a6c91817610053ff2428934080833f8457d3fb
jacopolegovini <legovini96@gmail.com>
2024-12-11 11:09:40 +0100

    premium star added

--- src/components/Dashboard/MainDashboardComponents/SponsorshipPage.vue
+++ src/components/Dashboard/MainDashboardComponents/SponsorshipPage.vue
@@ -11,47 +11,74 @@ export default {
 <template>
     <main class="container">
         <h2>Sponsorizzazione</h2>
-        <p>
-            Investi nelle tue competenze sponsorizzando il profilo!
-            <br>
-            Un profilo sponsorizzato compare nella homepage e viene sempre posizionato in cima nella pagina di ricerca.
-        </p>
-        <p>
+        <div class="sponsored-description">
+            <p>
+                Investi nelle tue competenze sponsorizzando il profilo!
+                <br>
+                Un profilo sponsorizzato compare nella homepage e viene sempre posizionato in cima nella pagina di
+                ricerca.
+            </p>
+            <p>
 
-        </p>
-        <h5>Scegli la tua sponsorizzazione:</h5>
+            </p>
+        </div>
+        <h4>Scegli la tua sponsorizzazione:</h4>
         <section class="sponsor-cards">
             <div class="sponsor-card card-bronze">
-                <p class="description">Sponsorizzazione garantita per 24 ore</p>
-                <p>2,99€</p>
+                <div class="card-description">
+                    <p>Garantito per 24 ore</p>
+                    <p>2,99€</p>
+                </div>
+                <div class="premium-star"><i class="fa-solid fa-star"></i></div>
             </div>
             <div class="sponsor-card card-silver">
-                <p class="description">Sponsorizzazione garantita per 72 ore</p>
+                <p class="card-description">Garantito per 72 ore</p>
+                <p>5,99€</p>
             </div>
             <div class="sponsor-card card-gold">
-                <p class="description">Sponsorizzazione garantita per 144 ore</p>
+                <p class="card-description">Garantito per 144 ore</p>
+                <p>9,99€</p>
             </div>
         </section>
     </main>
 </template>
 
 <style scoped>
+p {
+    margin: 0
+}
+
 h2 {
     margin-bottom: 40px;
     text-align: center;
+}
+
+h4 {
+    margin: 20px;
+    text-align: center;
+}
+
+.sponsored-description {
+    padding-bottom: 10px;
 }
 
 
 /* Cards */
 .sponsor-cards {
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
+    gap: 10px;
 }
 
 .sponsor-card {
+    margin-top: 10px;
     padding: 25px 35px;
     border-radius: 25px;
     color: white;
+    font-weight: bold;
+    /* box-shadow: 5px 5px 15px 1px black; */
+    position: relative;
+    flex-basis: 30%
 }
 
 .card-bronze {
@@ -63,6 +90,19 @@ h2 {
 }
 
 .card-gold {
-    background-color: #A56C41;
+    background-color: #FFCC00;
+}
+
+.fa-star {
+    display: inline;
+    font-size: 0.9rem;
+    background-color: white;
+    border-bottom-left-radius: 30%;
+    border: 1px solid white;
+    padding: 10px;
+    color: #FFCC00;
+    position: absolute;
+    top: 0px;
+    right: 0px;
 }
 </style>
