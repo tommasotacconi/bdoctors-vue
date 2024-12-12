788d14c790d91a8ceb33cd4a563d953bf811453f
jacopolegovini <legovini96@gmail.com>
2024-12-12 12:02:38 +0100

    loaded implemented in sponsorshipPage

--- src/components/Dashboard/MainDashboardComponents/SponsorshipPage.vue
+++ src/components/Dashboard/MainDashboardComponents/SponsorshipPage.vue
@@ -13,6 +13,7 @@ export default {
             cardBronze: false,
             cardSilver: false,
             cardGold: false,
+            loaded: false,
         }
     },
     methods: {
@@ -64,6 +65,7 @@ export default {
                     console.log(error);
                 })
         },
+        // Metodo non adoperato, inglobato nella chiamata api
         getTypeSponsorship() {
             if (this.sponsorships[0].id === 1) {
                 console.log('test')
@@ -80,11 +82,19 @@ export default {
 
         }
     },
+    computed: {
+        showLoader() {
+            setTimeout(() => {
+                this.loaded = true
+            }, 2000)
+
+        }
+    },
     created() {
         this.getApiProfiles()
     },
     mounted() {
-        // this.getTypeSponsorship()
+        this.showLoader
     }
 }
 </script>
@@ -92,70 +102,74 @@ export default {
 <template>
     <main class="container">
         <h2>Sponsorizzazione</h2>
-        <div class="is-not-sponsored" v-if="!sponsorship">
-            <div class="sponsored-description">
-                <p>
-                    Investi nelle tue competenze, sponsorizza il tuo profilo!
-                    <br>
-                    Un profilo sponsorizzato compare nella homepage e viene sempre posizionato in cima nella pagina di
-                    ricerca.
-                </p>
-                <p>
+        <div class="loader" v-if="!loaded"></div>
+        <div class="container-flex" v-else>
+            <div class="is-not-sponsored" v-if="!sponsorship">
+                <div class="sponsored-description">
+                    <p>
+                        Investi nelle tue competenze, sponsorizza il tuo profilo!
+                        <br>
+                        Un profilo sponsorizzato compare nella homepage e viene sempre posizionato in cima nella pagina
+                        di
+                        ricerca.
+                    </p>
+                    <p>
 
-                </p>
+                    </p>
+                </div>
+                <h3>Scegli la tua sponsorizzazione:</h3>
+                <section class="sponsor-cards">
+                    <button class="sponsor-card card-bronze" @click="getPriceBronze()">
+                        <div class="card-description">
+                            <p class="hour-sponsorship">Garantito per 24 ore</p>
+                            <p class="price">2,99€</p>
+                        </div>
+                        <div class="premium-star"><i class="fa-solid fa-star"></i></div>
+                    </button>
+                    <button class="sponsor-card card-silver" @click="getPriceSilver()">
+
+                        <div class="card-description">
+                            <p class="hour-sponsorship">Garantito per 72 ore</p>
+                            <p class="price">5,99€</p>
+                        </div>
+                        <div class="premium-star"><i class="fa-solid fa-star"></i></div>
+
+                    </button>
+                    <button class="sponsor-card card-gold" @click="getPriceGold()">
+                        <div class="card-description">
+                            <p class="hour-sponsorship">Garantito per 144 ore</p>
+                            <p class="price">9,99€</p>
+                        </div>
+                        <div class="premium-star"><i class="fa-solid fa-star"></i></div>
+                    </button>
+                </section>
             </div>
-            <h3>Scegli la tua sponsorizzazione:</h3>
-            <section class="sponsor-cards">
-                <button class="sponsor-card card-bronze" @click="getPriceBronze()">
+            <div class="is-sponsored" v-else>
+                <div class="sponsor-card card-bronze" v-if="cardBronze">
                     <div class="card-description">
-                        <p class="hour-sponsorship">Garantito per 24 ore</p>
-                        <p class="price">2,99€</p>
+                        <p class="hour-sponsorship">Il tuo profilo è sponsorizzato</p>
                     </div>
                     <div class="premium-star"><i class="fa-solid fa-star"></i></div>
-                </button>
-                <button class="sponsor-card card-silver" @click="getPriceSilver()">
-
+                </div>
+                <div class="sponsor-card card-silver" v-else-if="cardSilver">
                     <div class="card-description">
-                        <p class="hour-sponsorship">Garantito per 72 ore</p>
-                        <p class="price">5,99€</p>
+                        <p class="hour-sponsorship">Il tuo profilo è sponsorizzato</p>
                     </div>
                     <div class="premium-star"><i class="fa-solid fa-star"></i></div>
-
-                </button>
-                <button class="sponsor-card card-gold" @click="getPriceGold()">
+                </div>
+                <div class="sponsor-card card-gold" v-else-if="cardGold">
                     <div class="card-description">
-                        <p class="hour-sponsorship">Garantito per 144 ore</p>
-                        <p class="price">9,99€</p>
+                        <p class="hour-sponsorship">Il tuo profilo è sponsorizzato</p>
                     </div>
                     <div class="premium-star"><i class="fa-solid fa-star"></i></div>
-                </button>
-            </section>
-        </div>
-        <div class="is-sponsored" v-else>
-            <div class="sponsor-card card-bronze" v-if="cardBronze">
-                <div class="card-description">
-                    <p class="hour-sponsorship">Il tuo profilo è sponsorizzato</p>
                 </div>
-                <div class="premium-star"><i class="fa-solid fa-star"></i></div>
-            </div>
-            <div class="sponsor-card card-silver" v-else-if="cardSilver">
-                <div class="card-description">
-                    <p class="hour-sponsorship">Il tuo profilo è sponsorizzato</p>
-                </div>
-                <div class="premium-star"><i class="fa-solid fa-star"></i></div>
-            </div>
-            <div class="sponsor-card card-gold" v-else-if="cardGold">
-                <div class="card-description">
-                    <p class="hour-sponsorship">Il tuo profilo è sponsorizzato</p>
-                </div>
-                <div class="premium-star"><i class="fa-solid fa-star"></i></div>
             </div>
         </div>
     </main>
 </template>
 
 <style scoped>
-main {
+.container-flex {
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -270,5 +284,41 @@ button {
 
 .is-sponsored {
     width: 50%;
+}
+
+
+/* Loader progressive */
+.loader {
+    --r1: 154%;
+    --r2: 68.5%;
+    width: 60px;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    background:
+        radial-gradient(var(--r1) var(--r2) at top, #0000 79.5%, var(--color-secondary) 80%),
+        radial-gradient(var(--r1) var(--r2) at bottom, var(--color-secondary) 79.5%, #0000 80%),
+        radial-gradient(var(--r1) var(--r2) at top, #0000 79.5%, var(--color-secondary) 80%),
+        #ccc;
+    background-size: 50.5% 220%;
+    background-position: -100% 0%, 0% 0%, 100% 0%;
+    background-repeat: no-repeat;
+    animation: l9 2s infinite linear;
+    position: absolute;
+    top: 50%;
+    left: 59%;
+}
+
+@keyframes l9 {
+    33% {
+        background-position: 0% 33%, 100% 33%, 200% 33%
+    }
+
+    66% {
+        background-position: -100% 66%, 0% 66%, 100% 66%
+    }
+
+    100% {
+        background-position: 0% 100%, 100% 100%, 200% 100%
+    }
 }
 </style>
