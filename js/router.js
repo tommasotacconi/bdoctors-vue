e2a1b86aa078411183b3ac97ad32108951cc4943
tommmasotacconi <tommaso.tacconi@gmail.com>
2025-05-04 18:02:45 +0200

    merge branch 'router' into homepage

--- js/router.js
+++ js/router.js
@@ -8,8 +8,46 @@ import ProfileEdit from '../src/pages/ProfileEdit.vue'
 import Createpage from '../src/pages/Createpage.vue'
 import AdvancedSearch from '../src/pages/AdvancedSearch.vue'
 import DoctorShow from '../src/components/Search/DoctorShow.vue'
+import WithHeaderLayout from '../src/layouts/WithHeaderLayout.vue'
+
+import axios from 'axios'
+import { store } from './store'
 
 const routes = [
+	{ 
+		path: '/',
+		component: WithHeaderLayout,
+		children: [
+			{
+				path: '',
+				name: 'homepage',
+				components: {
+					default: AppMainHome,
+					header: AppHeader  
+				}
+			},
+			{ path: '/user/login',
+				name: 'login',
+				components: {
+					default: LoginPage,
+					header: AppHeader
+				}, 
+			},
+			{ path: '/user/register',
+				name: 'register',
+				components: {
+					default: RegisterPage,
+					header: AppHeader
+				}, 
+			},
+			{ path: '/user/:id',
+				name: 'dashboard',
+				components: {
+					default: Dashboard,
+				}, 
+			},
+		]
+	},
   { path: '/', component: Homepage, name: 'homepage' },
   { path: '/user/login', component: LoginPage, name: 'login' },
   { path: '/user/register', component: RegisterPage, name: 'register' },
