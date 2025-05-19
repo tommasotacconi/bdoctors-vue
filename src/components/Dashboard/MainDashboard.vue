b03149f139672967c69a735fc57f161adb50b1a8
tommmasotacconi <tommaso.tacconi@gmail.com>
2025-05-19 04:30:28 +0200

    fix: correct component new location in import in MainDashboard.vue

--- src/components/Dashboard/MainDashboard.vue
+++ src/components/Dashboard/MainDashboard.vue
@@ -1,47 +1,47 @@
 <script>
-import { store } from '../../../js/store';
-import InformationPage from './MainDashboardComponents/InformationPage.vue';
-import MessagePage from './MainDashboardComponents/MessagePage.vue';
-import ReviewPage from './MainDashboardComponents/ReviewPage.vue';
-import SponsorshipPage from './MainDashboardComponents/SponsorshipPage.vue';
-import StatisticPage from './MainDashboardComponents/StatisticPage/StatisticPage.vue';
+	import { store } from '../../../js/store';
+	import InformationPage from './MainDashboardComponents/InformationPage.vue';
+	import MessagePage from './MainDashboardComponents/MessagePage.vue';
+	import ReviewPage from './MainDashboardComponents/ReviewPage.vue';
+	import SponsorshipPage from './MainDashboardComponents/SponsorshipPage.vue';
+	import StatisticPage from './MainDashboardComponents/StatisticsPage.vue';
 
-export default {
-    data() {
-        return {
-            store
-        }
-    },
-    components: {
-        InformationPage,
-        MessagePage,
-        ReviewPage,
-        SponsorshipPage,
-        StatisticPage,
-    }
-}
+	export default {
+		data() {
+			return {
+				store
+			}
+		},
+		components: {
+			InformationPage,
+			MessagePage,
+			ReviewPage,
+			SponsorshipPage,
+			StatisticPage,
+		}
+	}
 </script>
 
 <template>
-    <div v-if="store.informationPage">
-        <InformationPage />
-    </div>
+	<div v-if="store.informationPage">
+		<InformationPage />
+	</div>
 
-    <div v-if="store.messagePage">
-        <MessagePage />
-    </div>
+	<div v-if="store.messagePage">
+		<MessagePage />
+	</div>
 
-    <div v-if="store.reviewPage">
-        <ReviewPage />
-    </div>
+	<div v-if="store.reviewPage">
+		<ReviewPage />
+	</div>
 
-    <div v-if="store.sponsorshipPage">
-        <SponsorshipPage />
-    </div>
+	<div v-if="store.sponsorshipPage">
+		<SponsorshipPage />
+	</div>
 
-    <div v-if="store.statisticPage">
-        <StatisticPage />
-    </div>
+	<div v-if="store.statisticPage">
+		<StatisticPage />
+	</div>
 </template>
 
 <style scoped></style>
