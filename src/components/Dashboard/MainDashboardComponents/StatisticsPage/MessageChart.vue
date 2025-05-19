2ceeed702a08f81c3c83e4d86f6441b338fddef8
tommmasotacconi <tommaso.tacconi@gmail.com>
2025-05-19 04:22:58 +0200

    scaffolding: Change folder `StaticPage` name and rename and move up `StatisticPage.vue`; change chart component to adapt to new store names

---
+++ src/components/Dashboard/MainDashboardComponents/StatisticsPage/MessageChart.vue
@@ -0,0 +1,69 @@
+<script>
+	import { store } from '../../../../../js/store.js';
+
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		BarElement,
+		CategoryScale,
+		LinearScale
+	} from 'chart.js'
+	import { Bar } from 'vue-chartjs'
+
+	ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+	export default {
+		name: 'App',
+		components: {
+			Bar
+		},
+		data() {
+			return {
+				store,
+				data: {
+				},
+				options: {
+					responsive: true,
+					plugins: {
+						legend: {
+							display: false
+						}
+					},
+					scales: {
+						y: {
+							ticks: {
+								stepSize: 1
+							}
+						}
+					}
+				}
+			}
+		},
+		methods: {
+
+		},
+	}
+</script>
+
+<template>
+	<h4>Messaggi ricevuti <span class="total-year">(Totale anno: {{ store.profileMessages.length }})</span>
+	</h4>
+	<div class="char">
+		<Bar :data="store.chartDataMessages" :options="options" />
+	</div>
+
+</template>
+
+<style scoped>
+	h4 {
+		text-align: center;
+		margin-bottom: 20px;
+	}
+
+	.total-year {
+		opacity: 0.8;
+		font-style: italic;
+	}
+</style>
