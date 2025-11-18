3608514c68887021405727e84d36c4d9fc091490
tommasotacconi <tommaso.tacconi@gmail.com>
2025-11-18 12:17:48 +0100

    fix: Config server to be used with Sanctum using same-origin cookie (same scheme and domain for frontend and backend)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import path from 'path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+	plugins: [vue()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
@@ -16,5 +17,13 @@ export default defineConfig({
 				additionalData: `@use "@/styles/_variables.scss" as vars;`
 			}
 		}
+	},
+	server: {
+		https: {
+			key: fs.readFileSync('certs/localhost+1-key.pem'),
+			cert: fs.readFileSync('certs/localhost+1.pem')
+		},
+		host: 'localhost',
+		port: 5173
 	}
 })
