f21b0f01b419b5a51719967e9ff7e97d1071b0c7
tommasotacconi <tommaso.tacconi@gmail.com>
2025-12-01 13:15:57 +0100

    feat: Use mode parameter to decide to switch on or not vite server

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,30 @@ import vue from '@vitejs/plugin-vue'
 import fs from 'fs'
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode, command }) => {
+	return {
+		plugins: [vue()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
 		},
-	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				additionalData: `@use "@/styles/_variables.scss" as vars;`
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `@use "@/styles/_variables.scss" as vars;`
+				}
 			}
-		}
-	},
-	server: {
-		https: {
-			key: fs.readFileSync('certs/localhost+1-key.pem'),
-			cert: fs.readFileSync('certs/localhost+1.pem')
 		},
-		host: 'localhost',
-		port: 5173
-	}
-})
+		server: mode === 'development'
+			? {
+				https: {
+					key: fs.readFileSync('certs/localhost+1-key.pem'),
+					cert: fs.readFileSync('certs/localhost+1.pem')
+				},
+				host: 'localhost',
+				port: 5173
+			}
+			: undefined
+		}
+	})
