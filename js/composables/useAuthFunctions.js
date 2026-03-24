3e0c6af03adf627ef9414ee443acb7450b0ae389
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-03-24 13:33:44 +0100

    refactor, style: Use new initial value for profileDataGeneral property at logout; prettier style changes

--- js/composables/useAuthFunctions.js
+++ js/composables/useAuthFunctions.js
@@ -4,24 +4,30 @@ import { store } from '../store';
 import { dashboardStore } from '../dashboardStore';
 
 export function useAuthFunctions() {
-    const isLoggingOut = ref(false);
+  const isLoggingOut = ref(false);
 
-    const logout = ({ inThenOperations, inCatchOperations, additionalOperations }) => {
-        axios.post(store.apiUri.slice(0, -4) + 'logout',
-            {
-                withCredentials: true
-            })
-            .then(() => {
-                store.isAuthenticated = false;
-                dashboardStore.profileDataGeneral = {};
-                inThenOperations?.();
-            })
-            .catch(() => {/* console.log(err); */ inCatchOperations?.();})
-            .finally(() => isLoggingOut.value = false);
+  const logout = ({
+    inThenOperations,
+    inCatchOperations,
+    additionalOperations,
+  }) => {
+    axios
+      .post(store.apiUri.slice(0, -4) + 'logout', {
+        withCredentials: true,
+      })
+      .then(() => {
+        store.isAuthenticated = false;
+        dashboardStore.profileDataGeneral = null;
+        inThenOperations?.();
+      })
+      .catch(() => {
+        /* console.log(err); */ inCatchOperations?.();
+      })
+      .finally(() => (isLoggingOut.value = false));
 
-        isLoggingOut.value = true;
-        additionalOperations?.();
-    }
+    isLoggingOut.value = true;
+    additionalOperations?.();
+  };
 
-    return { isLoggingOut, logout }
+  return { isLoggingOut, logout };
 }
